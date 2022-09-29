e5b0f8c746442f23c1d205a5533f7e730b95a92a
«VendyVL» <vedmansk@yandex.ru>
2022-09-29 09:04:59 +0300

    something new

--- 111/com.py
+++ 111/com.py
@@ -4,15 +4,16 @@ from search_number import Searching
 from delete_user import Deleting
 from print_all import Printing
 
-print('1 - добавить номер')
-print('2 - найти номер')
-print('3 - посмотреть всех')
-print('4 - удалить пользователя')
+def doing():
+    print('1 - добавить номер')
+    print('2 - найти номер')
+    print('3 - посмотреть всех')
+    print('4 - удалить пользователя')
 
-doing = input('Выберите действие: ')
+    doing = input('Выберите действие: ')
 
-if doing == '1': Write_num
-elif doing == '2':Searching
-elif doing == '3':Printing
-elif doing == '4':Deleting
-else: print('Ошибка')
+    if doing == '1': Write_num()
+    elif doing == '2':Searching()
+    elif doing == '3':Printing()
+    elif doing == '4':Deleting()
+    else: print('Ошибка')
---
+++ 111/main.py
@@ -0,0 +1,4 @@
+from HomeworkPython7.111.com import doing
+import com
+
+com.doing()
